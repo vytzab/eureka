e4c90e351c48cd10a19a0fd02f68cc99385a9499
vytzab <vytautas.zabielskas@gmail.com>
2024-04-23 09:44:01 +0300

    Initial commit

---
+++ src/test/java/com/vytzab/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vytzab.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
